96ebea12e532844db5a675b31c2766be5c0cac50
Denis Sobolev <sda141715@gmail.com>
2025-11-21 17:11:29 +0300

    Исправил виртуальную папку на физическую и переместил туда Решения

---
+++ Lesson1/CalculatorLibrary/MathCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public static class MathCalculator
+    {
+        /// <summary>
+        /// Складывает <paramref name="x"/> и <paramref name="y"/>
+        /// </summary>
+        /// <param name="x">Первый аргумент</param>
+        /// <param name="y">Второй аргумент</param>
+        /// <returns>сумму чисел</returns>
+        public static double Add(double x, double y) => x + y;
+
+        public static double Subtract(double x, double y) => x - y;
+
+        public static double Multiply(double x, double y) => x * y;
+
+        public static double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("Аргумент не должен быть нулем", 
+                    nameof(y));
+            }
+
+            return x / y;
+        }
+    }
+}
