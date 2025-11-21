125c8180beb1ee737e99b1d991c686aed4e1b8df
Denis Sobolev <sda141715@gmail.com>
2025-11-21 16:53:31 +0300

    Удалили ненужное косносльное приложение

---
+++ CalculatorLibrary/MathCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class MathCalculator
+    {
+        public double Add(double x, double y) => x + y;
+
+        public double Subtract(double x, double y) => x - y;
+
+        public double Multiply(double x, double y) => x * y;
+
+        public double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new ArgumentException("Аргумент не должен быть нулем", 
+                    nameof(y));
+            }
+
+            return x / y;
+        }
+    }
+}
